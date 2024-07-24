149916cb0144dd3b181bb01b79b0d51c88e57d7a
flyc <yayayahahahaooii@gmail.com>
2024-07-24 19:15:47 +0800

    install sass

--- vue-plain-project/src/pages/HomeView.vue
+++ vue-plain-project/src/pages/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>This is {{ $options.name }} Page</h1>
+  <h1>
+    This is <span>{{ $options.name }}</span> Page
+  </h1>
   <router-link to="/about">about</router-link>
 </template>
 
@@ -8,3 +10,13 @@ export default {
   name: 'HomeView',
 }
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  text-decoration: underline;
+
+  span {
+    text-decoration: line-through;
+  }
+}
+</style>
