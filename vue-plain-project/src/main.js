f3402d6c8400ac9f77c6bfc2ec85f552aaa04e88
flyc <yayayahahahaooii@gmail.com>
2024-07-24 19:10:06 +0800

    add vue-router

--- vue-plain-project/src/main.js
+++ vue-plain-project/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import store from '@/store/index.js'
+import router from '@/router/index.js'
 
 const app = createApp(App)
+
 app.use(store)
+app.use(router)
 
 app.mount('#app')
---
+++ vue-plain-project/src/pages/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>This is {{ $options.name }} Page</h1>
+  <router-link to="/about">about</router-link>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
