9c9c8f1e930cc65d779f54518d2238cd3e3bf63d
flyc <yayayahahahaooii@gmail.com>
2024-11-18 12:38:03 +0800

    tuning pull-request script detail

--- global-node-script/btse-pr.js
+++ global-node-script/btse-pr.js
@@ -4,7 +4,7 @@ const PROJECT_ID_MAP = {
   FRONTEND: '43',
 }
 
-const WHITE_LABEL_LIST =[
+const WHITE_LABEL_LIST = [
   'btse',
   'storybook',
   'paradise',
@@ -30,7 +30,7 @@ const WHITE_LABEL_LIST =[
   'binoex',
   'nvx',
   'traiex',
-  'autotrader'
+  'autotrader',
 ]
 
 const BASE_URL = 'https://gitlab01.oa.btse.io/btse/frontend/-/merge_requests/new'
@@ -39,7 +39,10 @@ function generateUrl(sourceBranch, { baseUrl = BASE_URL } = {}) {
   return `${baseUrl}?${generateQuery(sourceBranch)}`
 }
 
-function generateQuery(sourceBranch, { projectId = PROJECT_ID_MAP.FRONTEND, targetBranch = 'develop', title: originalTitle = null } = {}) {
+function generateQuery(
+  sourceBranch,
+  { projectId = PROJECT_ID_MAP.FRONTEND, targetBranch = 'develop', title: originalTitle = null } = {}
+) {
   const title = originalTitle ?? branchNameToPrTitle(sourceBranch)
 
   const payload = {
@@ -49,38 +52,41 @@ function generateQuery(sourceBranch, { projectId = PROJECT_ID_MAP.FRONTEND, targ
     'merge_request[target_branch]': targetBranch,
     'merge_request[description]': '掛上 draft 避免誤觸',
     'merge_request[assignee_ids][]': '397', // 這個是我自己
-    'merge_request[title]': `Draft: ${title}` // 讓 PR 是 draft
+    'merge_request[title]': `Draft: ${title}`, // 讓 PR 是 draft
   }
 
   return new URLSearchParams(payload).toString()
 }
 
 function branchNameToPrTitle(branchName) {
-  const [, num, originalName] = branchName.match(/(PLAT-\d+)_(.+)/) || ['','','']
+  const [, num, originalName] = branchName.match(/(PLAT-\d+)_(.+)/) || ['', '', '']
 
-  const { keys, last } = (originalName || branchName).split('-').reduce((payload, str) => {
-    switch (true) {
-      case str === 'FE':
-      case WHITE_LABEL_LIST.includes(str.toLocaleLowerCase()):
-        payload.keys.push(str)
-        break
-      default:
-        payload.last.push(str)
-    }
+  const { keys, last } = (originalName || branchName).split('-').reduce(
+    (payload, str) => {
+      switch (true) {
+        case str === 'FE':
+        case WHITE_LABEL_LIST.includes(str.toLocaleLowerCase()):
+          payload.keys.push(str)
+          break
+        default:
+          payload.last.push(str)
+      }
 
-    return payload
-  }, { keys: [], last: [] })
+      return payload
+    },
+    { keys: [], last: [] }
+  )
 
   const jiraNum = num
-  const keysText = keys.map(key => `[${key}]`)
+  const keysText = keys.map((key) => `[${key}]`).join('')
   const titleText = last.join(' ')
 
   return `${jiraNum} ${keysText} ${titleText}`.replace(/\s+/, ' ')
 }
 
 // start here
-;(function() {
-  const [,,branchName] = process.argv
+;(function () {
+  const [, , branchName] = process.argv
 
   if (branchName == null) return void console.log('branchName 不可為空')
 
