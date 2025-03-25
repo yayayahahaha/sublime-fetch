9f1549b0dc06f0aeed924da0588c490133945b6c
flyc <yayayahahahaooii@gmail.com>
2025-03-25 18:27:26 +0800

    add open jira url script

---
+++ global-node-script/btse-goto-jira.js
@@ -0,0 +1,49 @@
+const { exec } = require('child_process')
+
+const WHITE_LABEL_LIST = [
+  'btse',
+  'altex',
+  'autotrader',
+  'b2z',
+  'bestpay',
+  'binoex',
+  'bitkub',
+  'bitmarkets',
+  'bitmarketsalpha',
+  'bitqik',
+  'btseag',
+  'btsegi',
+  'btseuab',
+  'bullstreet',
+  'coinwise',
+  'cryptomarket',
+  'exchangedemo',
+  'fedhabit',
+  'interpay',
+  'lmex',
+  'nvx',
+  'obot',
+  'paradise',
+  'pixbit',
+  'testnet',
+  'traiex',
+  'transexchange',
+  'walletdemo',
+]
+
+function generateJiraUrl(branchName) {
+  const [, num] = branchName.match(/(PLAT-\d+)_.+/) || ['', '', '']
+
+  const jiraNum = num
+
+  return `https://btse.atlassian.net/browse/${jiraNum}`
+}
+
+// start here
+;(function () {
+  const [, , branchName] = process.argv
+
+  if (branchName == null) return void console.log('branchName 不可為空')
+
+  exec(`open '${generateJiraUrl(branchName)}'`)
+})()
